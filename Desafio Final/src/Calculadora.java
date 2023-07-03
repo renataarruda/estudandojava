d0789a397dab1b4c2621bfb1d464e23781833828
Renata Arruda <renata.arruda85@gmail.com>
2023-07-03 18:41:39 -0300

    style: alterando estilo do código e removendo variável desnecessária.

--- Desafio Final/src/Calculadora.java
+++ Desafio Final/src/Calculadora.java
@@ -6,7 +6,6 @@ public class Calculadora {
         double base, altura, raio;
         Scanner Entrada= new Scanner(System.in); //cria o buffer
         boolean continuar=true;
-        int opt;
         int figuraGeometrica;
 
         do {
@@ -18,35 +17,33 @@ public class Calculadora {
             figuraGeometrica = Entrada.nextInt();
 
             switch (figuraGeometrica) {
-                case 1:
+                case 1 -> {
                     System.out.println("Digite o valor da base(metros):");
                     base = Entrada.nextDouble();
                     System.out.println("Digite o valor da altura(metros):");
                     altura = Entrada.nextDouble();
                     TrianguloRetangulo triRet = new TrianguloRetangulo(base, altura);
                     System.out.println(triRet.mostraResultado());
-                    break;
-                case 2:
+                }
+                case 2 -> {
                     System.out.println("Digite o valor do raio(metros):");
                     raio = Entrada.nextDouble();
                     Circulo circ = new Circulo(raio);
                     System.out.println(circ.mostraResultado());
-                    break;
-                case 3:
+                }
+                case 3 -> {
                     System.out.println("Digite o valor da base(metros):");
                     base = Entrada.nextDouble();
                     System.out.println("Digite o valor da altura(metros):");
                     altura = Entrada.nextDouble();
                     Quadrado quad = new Quadrado(base, altura);
-                    System.out.println(quad.mostraResultado());;
-                    break;
-                case 4:
+                    System.out.println(quad.mostraResultado());
+                }
+                case 4 -> {
                     System.out.println("PROGRAMA FINALIZADO");
-                    continuar=false;
-                    break;
-                default:
-                    System.out.println("Dígito inválido.");
-                    break;
+                    continuar = false;
+                }
+                default -> System.out.println("Dígito inválido.");
             }
 
         } while(continuar);
